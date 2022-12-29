837476c777f89fe8ac173f6770353279d0aee1f5
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2022-12-29 21:41:23 +0530

    Generate Random Password

--- Email.java
+++ Email.java
@@ -5,6 +5,7 @@ public class Email {
     private String lastName;
     private String password;
     private String department ;
+    private int defaultPasswordLength = 10;
     private int mailBoxCapacity;
     private String alternateEmail;
 
@@ -18,11 +19,15 @@ public class Email {
         // Call a method asking for the Department - return the Department
         this.department = setDepartment();
         System.out.println("Department is :" + this.department );
+
+        // Call a methods which return the random password
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your Password : " + this.password );
     }
 
     // Ask for the Department
     private String setDepartment(){
-        System.out.print("Choose Department : \n1 for Sales \n2 for Development \n3 for Accounting \n0 for None \n Enter the Department : ");
+        System.out.print("DEPARTMENT CODES\n1 for Sales \n2 for Development \n3 for Accounting \n0 for None \n Enter Department CODE : ");
         
         Scanner sc = new Scanner(System.in);
 
@@ -36,6 +41,18 @@ public class Email {
     }
 
     //Generate a Random password 
+
+    private String randomPassword( int length ){
+
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$&*";
+
+        char[] password = new char[length];
+        for(int i=0 ; i<length ; i++){
+            int rand = ( int ) ( Math.random() * passwordSet.length() );
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String( password );
+    }
 
     // Set the mailbox Capacity
 
